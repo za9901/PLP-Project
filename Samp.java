7dac47e7334c983e8679a7e724bce8a2a00bfa4d
za9901 <51301649+za9901@users.noreply.github.com>
2019-06-19 14:33:08 +0530

    Add files via upload

---
+++ Samp.java
@@ -0,0 +1,19 @@
+class Samp1
+{
+
+Samp1()// default constructor// 
+{
+ system.out.println(" hello");
+}
+
+Samp1(int a,int b)// argumented constructor//
+{
+system.out.println(" how r u");
+}
+public static void main(string arg[])
+{
+Samp1 s1=new Samp1();
+Samp1 s2=new Samp1(10,20);// passing values to the object//
+}
+
+
